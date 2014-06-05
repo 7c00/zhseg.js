3f07499a781bd0ffe8a058c341daf4cd2845e782
Todd Gao <sohub79430341903@sohu.com>
2014-06-05 11:31:27 +0800

    完成fmm算法

--- src/zhseg.js
+++ src/zhseg.js
@@ -2,10 +2,10 @@
 
 var zhseg = (function(){
 
-    var delims = " ~!@#$%^&*()_+{}|:\"<>?`-=[]\\;',./";
-    delims += "~！@#￥%……&*（）——+『』|：“《》？·-=【】、；‘，。/";
-    delims += "　～！＠＃￥％……＆＊（）——＋『』｜：”《》";
-    delims += "？·－＝【】、；’，。／";
+    var DELIMS = " ~!@#$%^&*()_+{}|:\"<>?`-=[]\\;',./";
+    DELIMS += "~！@#￥%……&*（）——+『』|：“《》？·-=【】、；‘，。/";
+    DELIMS += "　～！＠＃￥％……＆＊（）——＋『』｜：”《》";
+    DELIMS += "？·－＝【】、；’，。／";
 
     var trie = new Trie();
 
@@ -16,32 +16,81 @@ var zhseg = (function(){
     };
 
     var seg = function(text){
-        var result = [];
+        return frag(text, algo_fmm);
+    };
+
+    var TYPE_DEFAULT = 0;
+    var TYPE_NUMBER  = 1;
+    var TYPE_LETTER  = 2;
+    var TYPE_DELIM   = 3;
+
+    var frag = function(text, algo){
+        console.log(text);
         var text_length = text.length;
-        var sentence_begin = 0;
-        var sentence_end = sentence_begin;
-
-        while(sentence_end < text_length){
-            var ch = text.charAt(sentence_end);
-            if(delims.indexOf(ch) == -1){
-                sentence_end += 1;
-                continue;
+        var window_begin = 0;
+        var window_end = 0;
+        var result = [];
+        var ltype = undefined;
+        var output_part = function(wbegin, wend){
+            var part = text.substring(wbegin, wend);
+            if(ltype == TYPE_DEFAULT && algo){
+                console.log(part);
+                algo(part).forEach(function(e,i){ result.push(e)});
+            }else{
+                result.push(part);
             }
-            var frag = text.substring(sentence_begin, sentence_end);
+        };
+        while(window_end < text_length){
+            var ch = text.charAt(window_end);
+            var is_number = ch >= '0' && ch <= '9';
+            var is_letter = (ch >= 'a' && ch <= 'z') ||
+                (ch >= 'A' && ch <= 'Z');
+            var is_delim = DELIMS.indexOf(ch) !== -1;
 
-            // TODO process frag
-            result.push(frag);
-            result.push(ch);
+            var ctype = TYPE_DEFAULT;
+            ctype = is_number ? TYPE_NUMBER : ctype;
+            ctype = is_letter ? TYPE_LETTER : ctype;
+            ctype = is_delim  ? TYPE_DELIM : ctype;
 
-            sentence_end += 1;
-            sentence_begin = sentence_end;
+            if(ltype == undefined){
+                ltype = ctype;
+                window_end += 1;
+            } else if(ltype == ctype){
+                window_end += 1;
+            } else if(ltype != ctype){
+                output_part(window_begin, window_end);
+                ltype = ctype;
+                window_begin = window_end;
+                window_end += 1;
+            }
+
+            if(window_end == text_length){
+                output_part(window_begin, window_end);
+                break;
+            }
+        }
+        return result;
+    };
+
+    var algo_fmm = function(part){
+        var result = [];
+        var part_length = part.length;
+        var window_begin = 0;
+        var window_end = 0;
+        // var tr = null;
+        while(window_end < part_length){
+            var ch = part.charAt(window_end);
+            var t = trie.find(ch);
+            while(t != null && window_end < part_length){
+                window_end += 1;
+                ch = part.charAt(window_end);
+                t = t.find(ch);
+            }
+            result.push(part.substring(window_begin, window_end));
+            window_begin = window_end;
         }
         return result;
     }
-
-    var algo_fmm = function(text_frag){
-
-    };
 
     return {
         // trie : trie,
